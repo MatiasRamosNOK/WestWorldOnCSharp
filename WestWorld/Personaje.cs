74566570ce90d0dce0d58adc48f51e9d25e3e502
MatiasRamosNOK <swhz05@protonmail.ch>
2019-11-17 00:03:31 -0300

    Clase Personaje creada, falta Anfitrion y Huesped

---
+++ WestWorld/Personaje.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WestWorld
+{
+    class Personaje
+    {
+        public int energia;
+
+        public int Energia{
+            set
+            {
+                energia = value;
+            }
+
+            get
+            {
+                return energia;
+            }
+        }
+        
+        public int Felicidad()
+        {
+            return 1; // devuelve por defecto
+        }
+
+        public double Rebeldia()
+        {
+            return 1 / Felicidad();
+        }
+
+        public bool Rebelde()
+        {
+            return Rebeldia() > 10;
+        }
+
+        public void Interactuar(Personaje personaje)
+        {
+            energia /= 2;
+        }
+
+        public void ConsecuenciasDeConocerEscenario(Escenario escenario)
+        {
+
+        }
+
+        public void InteractuarConMuchos(List<Personaje> personajes)
+        {
+            personajes.ForEach((personaje) => Interactuar(personaje));
+        }
+    }
+}
